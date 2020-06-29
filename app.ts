d8b9268e09541b020ad4ce22ebda29b6348bdc43
balinux <Ryojuniyantara@gmail.com>
2020-06-29 13:41:33 +0800

    update: app.ts

--- app.ts
+++ app.ts
@@ -2,5 +2,5 @@ import { serve } from "https://deno.land/std@0.59.0/http/server.ts";
 const s = serve({ port: 8080 });
 console.log("http://localhost:8080/");
 for await (const req of s) {
-  req.respond({ body: "Hello World\n" });
+  req.respond({ body: "Halo yho tie\n" });
 }
